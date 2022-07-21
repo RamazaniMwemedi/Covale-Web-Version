695185e8170966fd49ea50f54af8138c14126331
Ramazani Mwemedi <ramazanimwemedi234@gmail.com>
2022-07-21 17:11:16 +0300

    Added the meet component

---
+++ soga/pages/components/MeetSection.js
@@ -0,0 +1,13 @@
+import { Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import React from 'react'
+
+const MeetSection = () => {
+  return (
+    <Box sx={{backgroundColor:"white", marginLeft:"-64px"}}>
+      <Typography variant="h1">MeetRight</Typography>
+    </Box>
+  );
+}
+
+export default MeetSection
