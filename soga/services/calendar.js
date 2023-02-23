0cc14ac237a4b0eb6b659301666e49e93828ec86
RamazaniMwemedi <ramazanimwemedi234@gmail.com>
2023-02-23 05:18:50 +0300

    Added calendar services file which will be handling API requests for calendar

---
+++ soga/services/calendar.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { SERVER_ADDRESS } from "../config/index";
+
+// Get all chats
+const getEventsForCalendar = async (token) => {
+  const response = await axios.get(`${SERVER_ADDRESS}/api/v1/calendar/event`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  console.log("response.data", response);
+  return response.data;
+};
+
+// Create a new Event
+const createNewEventForCalendar = async (token, formData) => {
+  const response = await axios.post(
+    `${SERVER_ADDRESS}/api/v1/calendar/event/new`,
+    formData,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  return response.data;
+};
+
+export { getEventsForCalendar, createNewEventForCalendar };
