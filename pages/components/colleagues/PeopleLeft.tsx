30908661679eb0b9d4141d18202144330ac12c0e
Ramazani <ramazanimwemedi234@gmail.com>
2023-11-20 21:14:28 +0300

    Fix styling issue in PeopleLeft component

--- pages/components/colleagues/PeopleLeft.tsx
+++ pages/components/colleagues/PeopleLeft.tsx
@@ -30,14 +30,35 @@ const useStyles = makeStyles({
     marginTop: "7px",
   },
 });
+const closedMixin = (theme: any): React.CSSProperties => ({
+  //
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(23)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(35)} + 1px)`,
+    marginLeft: theme.spacing(8),
+  },
+  justifyContent: "spaceBetween",
+  borderLeft: `2px solid ${theme.colors.background1}`,
+  borderRight: `2px solid ${theme.colors.background1}`,
+  backgroundColor: theme.colors.background,
+});
 
 const Drawer = styled(MuiDrawer, {
   shouldForwardProp: (prop) => prop !== "open",
-})(({ open }) => ({
+})(({ theme, open }) => ({
   width: "15px",
   flexShrink: 0,
   whiteSpace: "nowrap",
   boxSizing: "border-box",
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
 }));
 
 export default function PeopleLeft() {
